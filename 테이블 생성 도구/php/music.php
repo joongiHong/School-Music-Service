99db92b871b43315051c49f342c6bcf5913f49ac
BoxBoy (Hong Joongi) <joongi1978@naver.com>
2019-03-16 01:25:56 +0900

    테이블 자동 생성 도구 업로드
    mysql에 테이블을 간편하게 생성할 수 있는 도구를 업로드 합니다.
    테이블 생성 도구 폴더속에 있는 php를 최상위 디렉토리에 업로드 후에
    지침에 따라 주시기 바랍니다.

---
+++ 테이블 생성 도구/php/music.php
@@ -0,0 +1,33 @@
+﻿<?php
+$db_host = "localhost"; 
+$db_user = "db 로그인 아이디"; 
+$db_passwd = "db 로그인 비밀번호";
+$db_name = "db 이름"; 
+
+// MySQL - DB 접속.
+$conn = mysqli_connect($db_host,$db_user,$db_passwd,$db_name);
+if (mysqli_connect_errno()){
+echo "<script>alert('Mysql 접속 실패! db 접속 정보 확인 요망!')</script>";
+exit;
+}
+
+// table 만들기
+$sql = "CREATE TABLE music
+(
+    number INT AUTO_INCREMENT,
+    name VARCHAR(50),
+    singer VARCHAR(50),
+    album VARCHAR(50),
+    sf VARCHAR(5),
+    time TIMESTAMP DEFAULT NOW(),
+    PRIMARY KEY(number)
+);";
+
+if (mysqli_query($conn,$sql)){
+echo "<script>alert('Mysql 테이블 생성 성공! 다음 테이블 생성 조치 바람!')</script>";
+exit;
+} else {
+echo "<script>alert('에러 발생! 스크립트 수정 요망!')</script>";
+exit;
+}
+?> 
