c05bda8cfa43a0f83896f6f8b3fa0ef84509b0da
BoxBoy (Hong Joongi) <joongi1978@naver.com>
2019-03-17 20:01:23 +0900

    학생, 관리자 생성 스크립트 업로드
    임시 테스트를 위한 임시 학생, 관리자 생성 스크립트 업로드.

---
+++ 학생, 관리자 생성 스크립트/testuser/student.php
@@ -0,0 +1,28 @@
+<?php
+header("Content-Type: text/html; charset=UTF-8");
+$db_host = "localhost"; 
+$db_user = "db 로그인 아이디"; 
+$db_passwd = "db 로그인 비밀번호";
+$db_name = "db 이름"; 
+
+// MySQL - DB 접속.
+$conn = mysqli_connect($db_host,$db_user,$db_passwd,$db_name);
+
+if (mysqli_connect_errno()){
+echo "<script>alert('Mysql 접속 실패! db 접속 정보 확인 요망!')</script>";
+exit;
+}
+
+mysqli_query($conn, "set session character_set_connection=utf8;");
+mysqli_query($conn, "set session character_set_results=utf8;");
+mysqli_query($conn, "set session character_set_client=utf8;");
+// table 만들기
+$sql ="INSERT INTO studentdoc VALUES(10101, '홍길동');";
+if (mysqli_query($conn,$sql)){
+echo "<script>alert('테스트 용 학생 추가 성공!')</script>";
+exit;
+} else {
+echo "<script>alert('에러 발생! 스크립트 수정 요망!')</script>";
+exit;
+}
+?> 
