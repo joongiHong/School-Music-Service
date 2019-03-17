e5ab4785255ae1a60b09b21046ee47729c47ff65
BoxBoy (Hong Joongi) <joongi1978@naver.com>
2019-03-17 20:00:21 +0900

    테이블 자동 생성 도구 2차 수정
    한글 깨짐 오류 해결.

--- 테이블 생성 도구/php/master.php
+++ 테이블 생성 도구/php/master.php
@@ -1,16 +1,15 @@
-﻿<?php
+<?php
+header("Content-Type: text/html; charset=UTF-8");
 $db_host = "localhost"; 
 $db_user = "db 로그인 아이디"; 
 $db_passwd = "db 로그인 비밀번호";
 $db_name = "db 이름"; 
-
 // MySQL - DB 접속.
 $conn = mysqli_connect($db_host,$db_user,$db_passwd,$db_name);
 if (mysqli_connect_errno()){
 echo "<script>alert('Mysql 접속 실패! db 접속 정보 확인 요망!')</script>";
 exit;
 }
-
 // table 만들기
 $sql = "CREATE TABLE master
 (
@@ -18,9 +17,8 @@ $sql = "CREATE TABLE master
     pass VARCHAR(300),
     PRIMARY KEY(num)
 );";
-
 if (mysqli_query($conn,$sql)){
-echo "<script>alert('Mysql 테이블 생성 성공! 다음 테이블 생성 조치 바람!')</script>";
+echo "<script>alert('Mysql 테이블 생성 성공! 인코딩 옵션 변경 바람!')</script>";
 exit;
 } else {
 echo "<script>alert('에러 발생! 스크립트 수정 요망!')</script>";
