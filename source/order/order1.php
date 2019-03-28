c6b557830c070cd6082e047b427b65a577c487c1
BoxBoy (Hong Joongi) <joongi1978@naver.com>
2019-03-28 22:32:00 +0900

    SOURCE Beta1 - 5-4-2차 수정
    학교 이름을 의미하는 schoolname 변수와 연동 진행.

--- source/order/order1.php
+++ source/order/order1.php
@@ -4,22 +4,12 @@ Require $_SERVER["DOCUMENT_ROOT"]."/serviceinfo.php";
 <!DOCTYPE html>
 <html>
     <head>
-        <title><?php echo $schoolname ?> 음악 신청 서비스 - 음악신청</title>
+        <title><?php echo $schoolname ?> 음악 신청 서비스 - 본인인증</title>
         <meta charset="UTF-8">
         <link rel="stylesheet" href="../css/all.css" type="text/css">
         <link rel="stylesheet" href="../css/button.css" type="text/css">
         <link rel="stylesheet" href="../css/order.css" type="text/css">
-        <script language="JavaScript" type="text/JavaScript">
-            function agree(){
-                var yn=window.confirm('개인정보 처리 방침에 동의하신 것으로 간주하고\n가입을 진행합니다.');
-                if(yn==true) {
-                    location.href="order1.php";
-                }
-                else {
-                    alert("미 동의시 본인인증이 불가능하여\n신청이 어렵습니다.")
-                }
-            }
-        </script>
+        <link rel="stylesheet" href="../css/textbox.css" type="text/css">
     </head>
     <body>
         <header>
@@ -58,16 +48,22 @@ Require $_SERVER["DOCUMENT_ROOT"]."/serviceinfo.php";
             <br>
             <hr>
             <br>
-            <h3>개인정보 처리 방침</h3>
+            <h3>학생 개인정보</h3>
             <br>
-            <textarea readonly style="width: 100%; height: 300px; resize: none;">
-<?php
-Require $_SERVER["DOCUMENT_ROOT"]."/serviceinfo.php";
-
-echo $pp
-?>
-            </textarea>
-            <button class="btn-1" onclick="agree()">인증하기 (동의)</button>
+            <form action="order2.php" method="POST">
+                <div class="form_div">
+                    <div class="textbox">
+                        <label for="studentnum">학번</label>&nbsp;&nbsp;
+                        <input type="number" id="studentnum" name="studentnum">
+                    </div>
+                    <div class="textbox">
+                        <label for="studentname">이름</label>&nbsp;&nbsp;
+                        <input type="text" id="studentname" name="studentname">
+                    </div>
+                </div>
+                <br>
+                <button type="submit" class="btn-1">인증</button>
+            </form>
         </div>
     </body>
 </html>
